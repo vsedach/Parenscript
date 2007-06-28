31ad390eea173789f9e79474491e7f96f352bbd6
Vladimir Sedach <vsedach@gmail.com>
2007-06-28 19:28:23 +0000

    Added divisors to ceiling, floor, round.

--- src/js-utils.lisp
+++ src/js-utils.lisp
@@ -14,7 +14,7 @@
 (def-js-maths
     (min (&rest nums) `(*math.min ,@nums))
     (max (&rest nums) `(*math.max ,@nums))
-    (ceiling (n) `(*math.ceil ,n))
+    (ceiling (n &optional divisor) `(*math.ceil ,(if divisor `(/ ,n ,divisor) n)))
     (abs (n) `(*math.abs ,n))
     (sin (n) `(*math.sin ,n))
     (cos (n) `(*math.cos ,n))
@@ -23,9 +23,9 @@
     (asin (n) `(*math.asin ,n))
     (atan (n) `(*math.atan ,n))
     (exp (n) `(*math.exp ,n))
-    (floor (n) `(*math.floor ,n))
+    (floor (n &optional divisor) `(*math.floor ,(if divisor `(/ ,n ,divisor) n)))
     (expt (base power) `(*math.pow ,base ,power))
-    (round (n) `(*math.round ,n))
+    (round (n &optional divisor) `(*math.round ,(if divisor `(/ ,n ,divisor) n)))
     (random (&optional upto) (if upto
                                  `(floor (* ,upto (*math.random)))
                                  '(*math.random))))

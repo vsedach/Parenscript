319d266376fc2cad235dc7b5f1cf802daf4df8fc
Vladimir Sedach <vsedach@gmail.com>
2007-08-12 15:43:11 +0000

    Redid package system unit tests.

--- t/package-system-tests.lisp
+++ t/package-system-tests.lisp
@@ -5,39 +5,20 @@
 
 (in-suite package-system-tests)
 
-(defpstest simple-variable-prefix ()
-  (progn
-    (defpackage test (:lisp-package :parenscript-test))
-    (defvar the-var))
-   "var TEST_theVar;")
+(defpackage "PSTSTPKG"
+  (:use #:parenscript))
 
-(defpstest no-global-variable-prefix ()
-  (progn
-    (defvar global::the-var)
-    (defvar global::global))
-   "var theVar; var global;")
+(test namespace1 ()
+  (setf (ps-package-prefix "PSTSTPKG") "prefix_")
+  (is (string= "prefix_var;" (normalize-js-code (ps pststpkg::var)))))
 
-(defpstest eval-when-returns-paren-form ()
-  (progn
-    (eval-when (:compile-toplevel)
-      `(global::sort-of-macro-like))
-    global::treat-me-right)
-  "sortOfMacroLike(); treatMeRight;")
+(common-lisp:in-package "PSTSTPKG")
 
-(defpstest javascript-operations ()
-  (progn
-    (+ 1 2 3 4)
-    (- 1 2 3 4)
-    nil t this false undefined)
-  "1 + 2 + 3 + 4; 1 - 2 - 3 - 4; null; true; this; false; undefined;")
-
-(defpstest def-keywords ()
-  (progn
-    (defun global::hello-world () (return 5)))
-  "function helloWorld() { return 5; };")
-
-(defpstest ps-js-reserved ()
-  (eval-when (:compile-toplevel)
-    (format nil "~A" (script-package-name (symbol-script-package 'defclass))))
-  "'JAVASCRIPT';")
+(ps-test::test-ps-js namespace-and-special-forms
+  (let ((foo (create :bar 1 not-a-keyword something)))
+    (return (and (not foo) (+ (slot-value foo bar) some-other-var))))
+  "        var prefix_foo =
+            { bar : 1, 
+              prefix_notAKeyword : prefix_something };
+        return !prefix_foo && prefix_foo[prefix_bar] + prefix_someOtherVar;")
 

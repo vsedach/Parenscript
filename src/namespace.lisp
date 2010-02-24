6de6cc9fb42568ce1ca99366d91b1171e9fdb1ad
Vladimir Sedach <vsedach@gmail.com>
2010-02-24 17:25:38 -0500

    Changed OBFUSCATE-PACKAGE to take closures for symbol-maps only.

--- src/namespace.lisp
+++ src/namespace.lisp
@@ -1,12 +1,13 @@
 (in-package "PARENSCRIPT")
 
-;;; Symbol obfuscation
 (defvar *obfuscated-packages* (make-hash-table))
 
 (defun obfuscate-package (package-designator &optional
-                          (symbol-map (make-hash-table)))
-  "symbol-map can either be a hash table or a closure that takes a
-symbol as its only argument."
+                          (symbol-map
+                           (let ((symbol-table (make-hash-table)))
+                             (lambda (symbol)
+                               (or #1=(gethash symbol symbol-table)
+                                   (setf #1# (ps-gensym "G")))))))
   (setf (gethash (find-package package-designator) *obfuscated-packages*)
         symbol-map))
 
@@ -14,27 +15,16 @@ symbol as its only argument."
   (remhash (find-package package-designator) *obfuscated-packages*))
 
 (defun maybe-obfuscate-symbol (symbol)
-  (if (and (symbol-package symbol)
-           (eq :external
-               (nth-value 1 (find-symbol (symbol-name symbol)
-                                         (symbol-package symbol)))))
-           symbol
-      (ctypecase (gethash (symbol-package symbol) *obfuscated-packages*)
-        (hash-table (let ((symbol-map (gethash (symbol-package symbol)
-                                               *obfuscated-packages*)))
-                      (or (gethash symbol symbol-map)
-                          (setf (gethash symbol symbol-map) (ps-gensym "G")))))
-        (function (funcall (gethash (symbol-package symbol)
-                                    *obfuscated-packages*)
-                           symbol))
-        (null symbol))))
+  (if (aand (symbol-package symbol)
+            (eq :external (nth-value 1 (find-symbol (symbol-name symbol) it))))
+      symbol
+      (aif (gethash (symbol-package symbol) *obfuscated-packages*)
+           (funcall it symbol)
+           symbol)))
 
-;;; Interface for printing identifiers
 (defvar *package-prefix-table* (make-hash-table))
 
 (defmacro ps-package-prefix (package)
-  "Place for storing a string to be prefixed to any symbols in the
-designated package when translating ParenScript code."
   `(gethash (find-package ,package) *package-prefix-table*))
 
 (defun symbol-to-js-string (symbol &optional (mangle-symbol-name t))
@@ -43,5 +33,5 @@ designated package when translating ParenScript code."
                                   #'symbol-name)
                               (maybe-obfuscate-symbol symbol))))
     (aif (ps-package-prefix (symbol-package symbol))
-         (format nil "~A~A" it symbol-name)
+         (concatenate 'string it symbol-name)
          symbol-name)))

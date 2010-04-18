bb072dea50582ce3352845391333bed4f933d3c0
Vladimir Sedach <vsedach@gmail.com>
2010-04-17 20:30:07 -0400

    Refactored symbol name mangling code.

--- src/utils.lisp
+++ src/utils.lisp
@@ -1,36 +1,34 @@
 (in-package "PARENSCRIPT")
 
-(let ((cache (make-hash-table :test 'eq)))
-  (defun symbol-name-to-js-string (symbol)
-    "Given a Lisp symbol or string, produces to a valid JavaScript
-identifier by following transformation heuristics case conversion. For
-example, paren-script becomes parenScript, *some-global* becomes
-SOMEGLOBAL."
-    (or (gethash symbol cache)
-        (setf (gethash symbol cache)
-              (let ((sym-name (symbol-name symbol))
-                    (no-case-conversion nil)
+(let ((cache (make-hash-table :test 'equal)))
+  (defun encode-js-identifier (identifier)
+    "Given a string, produces to a valid JavaScript identifier by
+following transformation heuristics case conversion. For example,
+paren-script becomes parenScript, *some-global* becomes SOMEGLOBAL."
+    (or (gethash identifier cache)
+        (setf (gethash identifier cache)
+              (let ((no-case-conversion nil)
                     (lowercase t)
                     (all-uppercase nil))
-                (when (and (not (eq symbol '[])) ;; HACK
-                           (find-if (lambda (x) (find x '(#\. #\[ #\]))) sym-name))
+                (when (and (not (string= identifier "[]")) ;; HACK
+                           (find-if (lambda (x) (find x '(#\. #\[ #\]))) identifier))
                   (warn "Symbol ~A contains one of '.[]' - this compound naming convention is no longer supported by Parenscript!"
-                        symbol))
+                        identifier))
                 (acond ((nth-value 1 (cl-ppcre:scan-to-strings "[\\*|\\+](.+)[\\*|\\+](.*)"
-                                                               sym-name :sharedp t))
+                                                               identifier :sharedp t))
                         (setf all-uppercase t
-                              sym-name (concatenate 'string (aref it 0) (aref it 1))))
-                       ((and (> (length sym-name) 1)
-                             (or (eql (char sym-name 0) #\+)
-                                 (eql (char sym-name 0) #\*)))
+                              identifier (concatenate 'string (aref it 0) (aref it 1))))
+                       ((and (> (length identifier) 1)
+                             (or (eql (char identifier 0) #\+)
+                                 (eql (char identifier 0) #\*)))
                         (setf lowercase nil
-                              sym-name (subseq sym-name 1)))
-                       ((and (> (length sym-name) 1)
-                             (char= #\: (char sym-name 0)))
+                              identifier (subseq identifier 1)))
+                       ((and (> (length identifier) 1)
+                             (char= #\: (char identifier 0)))
                         (setf no-case-conversion t
-                              sym-name (subseq sym-name 1))))
+                              identifier (subseq identifier 1))))
                 (with-output-to-string (acc)
-                  (loop for c across sym-name
+                  (loop for c across identifier
                      do (acond ((eql c #\-)
                                 (setf lowercase (not lowercase)))
                                ((position c "!?#@%+*/=:<>")

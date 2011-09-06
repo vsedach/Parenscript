vsedach/Parenscript

commit abf27a29f4bc6b42a48547e523785a5dbe5308ba

Fixed behavior for one-arg / operator (thanks to Leslie Polzer for the bug report).

src/special-operators.lisp
@@ -8,7 +8,6 @@
   +          ps-js:+
   -          ps-js:-
   *          ps-js:*
-  /          ps-js:/
   rem        ps-js:%
   and        ps-js:&&
   or         ps-js:\|\|
@@ -22,6 +21,11 @@
 
   funcall    ps-js:funcall
   )
+
+(define-expression-operator / (&rest args)
+  (if (cdr args)
+      `(ps-js:/ ,@args)
+      `(ps-js:/ 1 ,@args)))
 
 (define-expression-operator - (&rest args)
   (let ((args (mapcar #'compile-expression args)))

t/output-tests.lisp
@@ -2842,3 +2842,7 @@ function (x) {
   "function (x) {
     return x + x;
 };")
+
+(test-ps-js divide-one-arg-reciprocal
+  (/ 2)
+  "1 / 2;")

commit f1009119146f86654c82a7f86c3dc58372b036f3

Fixed division bug introduced in abf27a29f. Thanks to Scott Bell and Daniel Gackle for the bug report.

src/special-operators.lisp
@@ -23,9 +23,7 @@
   )
 
 (define-expression-operator / (&rest args)
-  (if (cdr args)
-      `(ps-js:/ ,@args)
-      `(ps-js:/ 1 ,@args)))
+  `(ps-js:/ ,@(unless (cdr args) (list 1)) ,@(mapcar #'compile-expression args)))
 
 (define-expression-operator - (&rest args)
   (let ((args (mapcar #'compile-expression args)))

t/output-tests.lisp
@@ -2854,3 +2854,11 @@ function (x) {
 (test-ps-js division-not-associative
   (/ a (* b c))
   "a / (b * c);")
+
+(test-ps-js divide-expressions
+  (/ (foo) (bar))
+  "foo() / bar();")
+
+(test-ps-js divide-expressions1
+  (floor (1- x) y)
+  "Math.floor((x - 1) / y);")